9702174955c768cf0f3588decacb767197a6b07d
Crhistian David Vergara Gomez <crhistian.vergara@8base.com>
2023-10-29 08:46:28 -0500

    Initial Commit

---
+++ Yape Recipes/Yape_RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yape_RecipesApp.swift
+//  Yape Recipes
+//
+//  Created by Crhistian David Vergara Gomez on 29/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Yape_RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Yape RecipesUITests/Yape_RecipesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Yape_RecipesUITestsLaunchTests.swift
+//  Yape RecipesUITests
+//
+//  Created by Crhistian David Vergara Gomez on 29/10/23.
+//
+
+import XCTest
+
+final class Yape_RecipesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
